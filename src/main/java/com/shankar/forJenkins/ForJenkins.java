207c116d3e78e9d8d37ef6226b8cb86c8289ae77
Singa Selva Sankar <singaselva@archimedis.net>
2018-12-04 12:40:01 +0530

    second commit

---
+++ src/main/java/com/shankar/forJenkins/ForJenkins.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.shankar.forJenkins;
+
+/**
+ * @author sss
+ *
+ */
+public class ForJenkins {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Get life");
+
+	}
+
+}
